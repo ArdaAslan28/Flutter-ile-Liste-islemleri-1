74af3d2fe5f341aae22c6b42c1b9ee75471fb614
arda288 <70218432+arda288@users.noreply.github.com>
2023-03-27 23:24:53 +0300

    Add files via upload

---
+++ lib/liste.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+import 'package:myapp/main.dart';
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<int> liste= [0,0,0,0,0,0,0,0,0,0];
+  @override
+  Widget build(BuildContext context) {
+
+    void yokEt()
+    {
+      //ayarlanacak
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+      
+                      TextButton(onPressed: () {
+              Random rnd = Random();
+              setState(() {
+                liste = List.generate(10, (index) => rnd.nextInt(100));
+              });
+            }, child: const Card(
+              color: Colors.white,
+              child: Text('Liste Üret'),
+            ),),
+      
+            Flexible(
+              child: ListView.builder(
+                itemCount: liste.length,
+                itemBuilder: (context, index) {
+                return Card(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(liste[index].toString()),
+                    trailing: Container(
+                      width: 70,
+                      child: Row(
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [Expanded(child: IconButton(onPressed: () {
+                          setState(() {
+                            liste.removeAt(index);
+                          });
+                        }, icon: Icon(Icons.delete)))],
+                      ),
+                    ),
+                  ),
+                );
+              },),
+            ),
+      
+          ],
+        ),);
+  }
+}
